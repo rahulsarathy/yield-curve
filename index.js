88bbd8f786cb2c86234809ca735b9a4bb19722a5
Rahul Sarathy <rahul@sarathy.org>
2019-07-04 23:20:23 -0700

    fix syntax error

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ window.onload = function () {
     var to_insert = document.getElementById("chart");
 new Chart(to_insert, {
 	type: 'line',
-	    labels: [2018, 2019, 2020],
-	    datasets: 
-	    options : {
+	    labels: [2018, 2019, 2020], 
+	    options: {
 	    title: {
 		display: true,
 		    text: 'Line graph',
