bfb51247c326b75e0dc43668c115bd2642d68388
Rohit Sarathy <rohit@sarathy.org>
2019-07-07 21:31:57 -0700

    Bind bond yield data to API endpoints with DjangoRESTFramework.
    We bind bond yield data to URLs of the form /api/v1/bond_yield/YYYYMMDD. By making GET requests to these URLs, we return the bond yield data in the form a JSON object.

--- charting/urls.py
+++ charting/urls.py
@@ -4,4 +4,5 @@ from charting import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'api/v1/bond_yield/(?P<date>[0-9]{8})$', views.BondYieldData.as_view(), name='bond-yield-retrieve'),
 ]
