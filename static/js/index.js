e2c10365b661e88c5d89d9ed6dbb03754d1191e4
Rahul Sarathy <rahul@sarathy.org>
2019-07-08 23:51:08 -0700

    rudimentary charting

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,22 @@
-var data = [{
-
+var chart_data = [{
+  t: "2019-01-15",
+  y: 12
+}, {
+  t: "2019-02-25",
+  y: 21
+}, {
+  t: "2019-03-25",
+  y: 32
 }]
+
+var yield_curve;
 
 window.onload = function() {
 
   initDatePicker()
   initChart()
 
-  
+
 
 }
 
@@ -26,34 +35,71 @@ function initDatePicker() {
   });
 }
 
-function getBondData(date)
-{
-$.ajax({
-  url: 'api/v1/bond_yield/' + date,
-  success: function(data) {
-    console.log(data)
+function getBondData(date) {
+  $.ajax({
+      url: 'api/v1/bond_yield/' + date,
+      success: function(data) {
+        console.log(data)
+        year = parseInt(date.substring(0, 4))
+        month = parseInt(date.substring(4, 6))
+        day = parseInt(date.substring(6, 8))
+        var today = new Date(year, month, day)
+        var proper_dates = {
+          one_month: new Date(year, month + 1, day),
+          two_month: new Date(year, month + 2, day),
+          three_month: new Date(year, month + 3, day),
+          six_month: new Date(year, month + 6, day),
+          one_year: new Date(year + 1, month, day),
+          two_year: new Date(year + 2, month, day),
+          three_year: new Date(year + 3, month, day),
+          five_year: new Date(year + 5, month, day),
+          seven_year: new Date(year + 7, month, day),
+          ten_year: new Date(year + 10, month, day),
+          twenty_year: new Date(year + 20, month, day),
+          thirty_year: new Date(year + 30, month, day)
+        }
 
-  }
-});
+
+        var new_data = ['one_month', 'two_month', 'three_month', 'six_month', 'one_year', 'three_year',
+          'five_year', 'seven_year', 'ten_year', 'twenty_year', 'thirty_year']
+
+        var to_add = []
+
+        new_data.forEach(function(element) {
+          var wanted_date = proper_dates[element]
+          var date_string = wanted_date.getFullYear() + '-' + (wanted_date.getMonth() + 1) + '-' + wanted_date.getDate();
+          to_add.push(
+          {
+            t: date_string,
+            y: data[element]
+          })
+        });
+
+        addData(yield_curve, to_add)
+
+      }
+
+      // addData(yield_curve, {
+      //   t: date_string,
+      //   y: 10
+      // })
+  });
+}
+
+function addData(chart, data) {
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data = data
+  });
+  chart.update()
 }
 
 function initChart() {
-  var chart = new Chart(document.getElementById("chart"), {
+  yield_curve = new Chart(document.getElementById("chart"), {
     type: 'line',
     data: {
-      labels: ["2019-01-01T13:03:00Z", "2019-01-01T14:12:00Z"],
       datasets: [{
         label: 'Daily Treasury Yield Curve Rates',
-        data: [{
-          t: "2019-01-15",
-          y: 12
-        }, {
-          t: "2019-02-25",
-          y: 21
-        }, {
-          t: "2019-03-25",
-          y: 32
-        }],
+        data: chart_data,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
         ],
@@ -75,4 +121,3 @@ function initChart() {
     }
   });
 }
-
