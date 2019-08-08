b25fbaf71f6fe391a098c2dd5aef59daa0cff2f2
Rohit Sarathy <rohit@sarathy.org>
2019-08-07 22:07:54 -0700

    Add unit tests for the compounding calculator.

--- charting/tests.py
+++ charting/tests.py
@@ -49,9 +49,20 @@ class CompoundCalculatorTests(APITestCase):
     response = self.client.get(TEST_ROOT + '/api/v1/compound_calculator/?initial_value=1000&monthly_contribution=100&annual_growth=1.08')
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     data = response.json()
-    self.assertEqual(data['1 year']['Deposits'], 2200)
-    self.assertEqual(data['1 year']['Return'], 123.39)
-    self.assertEqual(data['10 year']['Deposits'], 13000)
-    self.assertEqual(data['10 year']['Return'], 7171.35)
-    self.assertEqual(data['30 year']['Deposits'], 37000)
-    self.assertEqual(data['30 year']['Deposits'], 113917.72)
+
+    # At the start, the total deposits should be equal to the initial deposit,
+    # and the total return should be 0.
+    self.assertEqual(data['0 years']['Deposits'], 1000)
+    self.assertEqual(data['0 years']['Return'], 0)
+
+    # 1 year
+    self.assertEqual(data['1 years']['Deposits'], 2200)
+    self.assertEqual(data['1 years']['Return'], 123.39)
+
+    # 10 years
+    self.assertEqual(data['10 years']['Deposits'], 13000)
+    self.assertEqual(data['10 years']['Return'], 7171.35)
+
+    # 30 years
+    self.assertEqual(data['30 years']['Deposits'], 37000)
+    self.assertEqual(data['30 years']['Return'], 113917.72)
