0a284cba537b5a60588699a2c1878ba1d67b683c
Rohit Sarathy <rohit@sarathy.org>
2019-09-02 13:24:27 -0700

    Include credit_cards URLs in the central config

--- siteconfig/urls.py
+++ siteconfig/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import include, url
 urlpatterns = [
   url(r'^', include('charting.urls')),
   url(r'^', include('compound.urls')),
+  url(r'^', include('credit_cards.urls')),
   url(r'^', include('investing.urls')),
   url(r'^', include('taxes.urls')),
 ]
